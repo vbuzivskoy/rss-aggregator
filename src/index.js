a0e1755b2cd41b7a72d5990d2205392546f10218
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-06-10 11:13:11 +0300

    Add index.js and index.html

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+document.querySelector('form.rss-feed').addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log('Add button clicked.');
+});
