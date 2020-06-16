15db0e15a2417f32b9d63f28c5cbd1045cb9b764
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-06-16 16:24:17 +0300

    Change tests

--- __tests__/parseRssFeed.test.js
+++ __tests__/parseRssFeed.test.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { parseRssFeed } from '../src/app';
+import parseRssFeed from '../src/parseRssFeed';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
@@ -10,18 +10,20 @@ test('parseRssFeed valid xml', () => {
     channel: {
       title: 'Lorem ipsum feed for an interval of 1 minutes with 3 items',
       description: 'This is a constantly updating lorem ipsum feed',
-      link: 'http://example.com/',
     },
     posts: [
       {
+        guid: 'http://example.com/test/1591780500',
         title: 'Lorem ipsum 2020-06-10T09:15:00Z',
         link: 'http://example.com/test/1591780500',
       },
       {
+        guid: 'http://example.com/test/1591780440',
         title: 'Lorem ipsum 2020-06-10T09:14:00Z',
         link: 'http://example.com/test/1591780440',
       },
       {
+        guid: 'http://example.com/test/1591780380',
         title: 'Lorem ipsum 2020-06-10T09:13:00Z',
         link: 'http://example.com/test/1591780380',
       },
