9fee1c46e2b320c0b2120f878ef02b434ccc6494
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-06-16 13:30:57 +0300

    Add translations

---
+++ src/locales/en.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    rssFeedForm: {
+      inputPlaceholder: 'RSS link',
+      addButton: 'Add',
+    },
+    errors: {
+      rssAlreadyAdded: 'This RSS feed has already been added',
+      notEmpty: 'The field should not be empty',
+      notURL: 'The field should contains valid URL',
+      networkError: 'Network error',
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+  translation: {
+    rssFeedForm: {
+      inputPlaceholder: 'RSS ссылка',
+      addButton: 'Добавить',
+    },
+    errors: {
+      rssAlreadyAdded: 'Такая RSS лента уже добавлена',
+      notEmpty: 'Поле не должно быть пустым',
+      notURL: 'Поле должно содержать верный URL',
+      networkError: 'Ошибка сети',
+    },
+  },
+};
