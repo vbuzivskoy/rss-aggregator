681413267b0ebd9dabe2bd4166047fd452fd2def
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-06-19 13:01:25 +0300

    Remove i18n initialization from view

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import i18next from 'i18next';
 import * as yup from 'yup';
 import differenceWith from 'lodash/differenceWith';
 import some from 'lodash/some';
 import parseRssFeed from './parseRssFeed';
 import watchState from './watchers';
+import en from './locales/en';
+import ru from './locales/ru';
 
 const corsProxyInit = () => {
   const corsAPIhost = 'cors-anywhere.herokuapp.com';
@@ -66,65 +69,72 @@ const app = () => {
     currentLocale: 'ru',
   };
 
-  watchState(state)
-    .then((result) => {
-      const watchedState = result;
-      const rssFeedForm = document.querySelector('#rssFeedForm');
-      const localeDropdownMenu = document.querySelector('#localeDropdownMenu');
+  i18next.init({
+    lng: state.currentLocale,
+    resources: {
+      en,
+      ru,
+    },
+  }).then(() => (
+    watchState(state)
+  )).then((result) => {
+    const watchedState = result;
+    const rssFeedForm = document.querySelector('#rssFeedForm');
+    const localeDropdownMenu = document.querySelector('#localeDropdownMenu');
 
-      const schema = yup.object().shape({
-        feedUrl: yup.string().url('notURL').required('notEmpty'),
-      });
+    const schema = yup.object().shape({
+      feedUrl: yup.string().url('notURL').required('notEmpty'),
+    });
 
-      rssFeedForm.addEventListener('input', (e) => {
-        watchedState.rssFeedForm.inputStatus = 'typing';
-        const formData = new FormData(e.currentTarget);
-        const feedUrl = formData.get('feedUrl');
-        schema.validate({ feedUrl })
-          .then(() => {
-            watchedState.rssFeedForm.validationStatus = 'valid';
-            watchedState.rssFeedForm.submitButtonStatus = 'enabled';
-            watchedState.rssFeedForm.validationErrors = [];
-          })
-          .catch(({ errors }) => {
-            watchedState.rssFeedForm.validationStatus = 'invalid';
-            watchedState.rssFeedForm.submitButtonStatus = 'disabled';
-            watchedState.rssFeedForm.validationErrors = errors;
-          });
-      });
-      rssFeedForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const feedUrl = formData.get('feedUrl');
-        const isAlreadyAdded = some(state.rssChannels, { channelUrl: feedUrl });
-        if (isAlreadyAdded) {
+    rssFeedForm.addEventListener('input', (e) => {
+      watchedState.rssFeedForm.inputStatus = 'typing';
+      const formData = new FormData(e.currentTarget);
+      const feedUrl = formData.get('feedUrl');
+      schema.validate({ feedUrl })
+        .then(() => {
+          watchedState.rssFeedForm.validationStatus = 'valid';
+          watchedState.rssFeedForm.submitButtonStatus = 'enabled';
+          watchedState.rssFeedForm.validationErrors = [];
+        })
+        .catch(({ errors }) => {
           watchedState.rssFeedForm.validationStatus = 'invalid';
           watchedState.rssFeedForm.submitButtonStatus = 'disabled';
-          watchedState.rssFeedForm.validationErrors = ['rssAlreadyAdded'];
-        } else {
-          getRssFeed(feedUrl)
-            .then((response) => {
-              const parsedRssFeedData = parseRssFeed(response.data);
-              watchedState.rssFeedForm.inputStatus = 'added';
-              watchedState.rssFeedForm.validationErrors = [];
-              watchedState.rssFeedForm.submitButtonStatus = 'disabled';
-              watchedState.rssChannels = [
-                { ...parsedRssFeedData.channel, channelUrl: feedUrl },
-                ...watchedState.rssChannels,
-              ];
-              watchNewPosts(watchedState);
-            })
-            .catch(() => {
-              watchedState.rssFeedForm.validationErrors = ['networkError'];
-            });
-        }
-      });
-
-      localeDropdownMenu.addEventListener('click', (e) => {
-        e.preventDefault();
-        watchedState.currentLocale = e.target.textContent;
-      });
+          watchedState.rssFeedForm.validationErrors = errors;
+        });
     });
+    rssFeedForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const feedUrl = formData.get('feedUrl');
+      const isAlreadyAdded = some(state.rssChannels, { channelUrl: feedUrl });
+      if (isAlreadyAdded) {
+        watchedState.rssFeedForm.validationStatus = 'invalid';
+        watchedState.rssFeedForm.submitButtonStatus = 'disabled';
+        watchedState.rssFeedForm.validationErrors = ['rssAlreadyAdded'];
+      } else {
+        getRssFeed(feedUrl)
+          .then((response) => {
+            const parsedRssFeedData = parseRssFeed(response.data);
+            watchedState.rssFeedForm.inputStatus = 'added';
+            watchedState.rssFeedForm.validationErrors = [];
+            watchedState.rssFeedForm.submitButtonStatus = 'disabled';
+            watchedState.rssChannels = [
+              { ...parsedRssFeedData.channel, channelUrl: feedUrl },
+              ...watchedState.rssChannels,
+            ];
+            watchNewPosts(watchedState);
+          })
+          .catch(() => {
+            watchedState.rssFeedForm.validationErrors = ['networkError'];
+          });
+      }
+    });
+
+    localeDropdownMenu.addEventListener('click', (e) => {
+      e.preventDefault();
+      watchedState.currentLocale = e.target.textContent;
+    });
+  });
 };
 
 export default app;
