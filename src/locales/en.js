f11ff7e81be5aaded4020c3cebf768e545b16493
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-06-24 13:13:13 +0300

    Add app title locales

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,14 +1,16 @@
 export default {
   translation: {
+    appTitle: 'RSS Aggregator',
     rssFeedForm: {
       inputPlaceholder: 'RSS link',
       addButton: 'Add',
     },
     errors: {
-      rssAlreadyAdded: 'This RSS feed has already been added',
+      notUniq: 'This RSS feed has already been added',
       notEmpty: 'The field should not be empty',
       notURL: 'The field should contains valid URL',
       networkError: 'Network error',
+      parserError: 'Invalid RSS format',
     },
   },
 };
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,14 +1,16 @@
 export default {
   translation: {
+    appTitle: 'RSS агрегатор',
     rssFeedForm: {
       inputPlaceholder: 'RSS ссылка',
       addButton: 'Добавить',
     },
     errors: {
-      rssAlreadyAdded: 'Такая RSS лента уже добавлена',
+      notUniq: 'Такая RSS лента уже добавлена',
       notEmpty: 'Поле не должно быть пустым',
       notURL: 'Поле должно содержать верный URL',
       networkError: 'Ошибка сети',
+      parserError: 'Неверный RSS формат',
     },
   },
 };
